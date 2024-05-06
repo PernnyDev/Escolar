8a2905d0b3530fa7ff8c2eef202a39b1993e34c9
Vinicius Nascimento Ribeiro <vinicius.ribeiro44@fatec.sp.gov.br>
2024-05-05 22:56:31 -0300

    chore: Remover arquivos e código desnecessários

--- scr/models/db.js
+++ scr/models/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 const url = "mongodb://127.0.0.1/escola";
-mongoose.connect(url).then(() => {
-console.log("MongoDB Conectado..");
-}).catch((err) => {
-console.log("Erro ao conectar: " + err);
-});
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("MongoDB Conectado..");
+  })
+  .catch((err) => {
+    console.log("Erro ao conectar: " + err);
+  });
